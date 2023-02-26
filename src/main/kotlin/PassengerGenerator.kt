de6584e5d459b2a754b7aea10d44e4e2f0f6cb77
uorri <2405490Gitlub>
2023-02-26 16:05:08 +0300

    Add an error handler

--- src/main/kotlin/PassengerGenerator.kt
+++ src/main/kotlin/PassengerGenerator.kt
@@ -1,6 +1,15 @@
 object PassengerGenerator {
-    fun getPassenger(name: String, floor: Int, target: Int): Passenger {
-        return Passenger(name, floor, target)
+    fun getPassenger(name: String, sourceFloor: Int, targetFloor: Int): Passenger {
+        if (name.isBlank() || name.isEmpty()) {
+            throw IllegalArgumentException("Invalid name: $name")
+        }
+        if (sourceFloor < Building.firstFloor || sourceFloor > Building.lastFloor) {
+            throw IllegalArgumentException("Invalid source floor: $sourceFloor")
+        }
+        if (targetFloor < Building.firstFloor || targetFloor > Building.lastFloor) {
+            throw IllegalArgumentException("Invalid target floor: $sourceFloor")
+        }
+        return Passenger(name, sourceFloor, targetFloor)
     }
 
 }
