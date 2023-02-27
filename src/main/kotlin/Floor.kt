dbbaee7e508ce0c09cd37a251ecec1cb88e2477d
uorri <2405490Gitlub>
2023-02-27 13:19:01 +0300

    Add buttons and door open state

--- src/main/kotlin/Floor.kt
+++ src/main/kotlin/Floor.kt
@@ -1,4 +1,20 @@
 data class Floor(
-    private val number: Int,
+    val number: Int,
     private val notifier: Notifier
-)
+) {
+    val buttonUp = object : FloorButton {
+        override fun click() {
+            ElevatorSystem.request(number, Direction.UP)
+        }
+    }
+    val buttonDown = object : FloorButton {
+        override fun click() {
+            ElevatorSystem.request(number, Direction.DOWN)
+        }
+    }
+
+    var isElevatorOnFloorAndDoorsAreOpen = false
+
+    fun getBarrier() = notifier.barriers[number - 1].barrier
+
+}
