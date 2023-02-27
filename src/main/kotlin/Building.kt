2d32a261d89273b796917699be34e22ba2d8f5a0
uorri <2405490Gitlub>
2023-02-27 13:20:56 +0300

    Add an array of floors

--- src/main/kotlin/Building.kt
+++ src/main/kotlin/Building.kt
@@ -1,6 +1,20 @@
 object Building {
-
     const val lastFloor: Int = 25
     const val firstFloor: Int = 1
+
+    private val floors = initFloors()
+    private val notifier = Notifier()
+
+    private fun initFloors(): Array<Floor> {
+        var i = 1
+        return Array(25) { Floor(i++, notifier) }
+    }
+
+    fun getSourceFloor(passenger: Passenger) = getFloor(passenger.sourceFloorNum)
+
+    fun getTargetFloor(passenger: Passenger) = getFloor(passenger.targetFloorNum)
+
+    private fun getFloor(number: Int) = floors[number - 1]
+
 
 }
