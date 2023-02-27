2c982336df81a72817ed12535e5687fc0023ad65
uorri <2405490Gitlub>
2023-02-27 13:23:49 +0300

    Add logic for emulating a passenger ride in the elevator

--- src/main/kotlin/PassengerEmulator.kt
+++ src/main/kotlin/PassengerEmulator.kt
@@ -1,7 +1,34 @@
 object PassengerEmulator {
 
     fun emulate(passenger: Passenger) {
+        val sourceFloor = Building.getSourceFloor(passenger)
+        passenger.callElevator(sourceFloor)
+        while (!sourceFloor.isElevatorOnFloorAndDoorsAreOpen) {
+            sourceFloor.getBarrier().await()
+        }
+        passenger.enterElevator()
+        val targetFloor = Building.getTargetFloor(passenger)
+        while (!targetFloor.isElevatorOnFloorAndDoorsAreOpen) {
+            targetFloor.getBarrier().await()
+        }
+        passenger.exitElevator(targetFloor)
+    }
+
+    private fun Passenger.callElevator(sourceFloor: Floor) {
+        if (this.sourceFloorNum < this.targetFloorNum) {
+            sourceFloor.buttonUp.click()
+        } else {
+            sourceFloor.buttonDown.click()
+        }
+    }
+
+    private fun Passenger.exitElevator(targetFloor: Floor) {
+        currentFloor = targetFloor.number
+    }
+
+    private fun Passenger.enterElevator() {
 
     }
+
 
 }
