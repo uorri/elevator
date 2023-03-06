dfc29b4eb1efc57e9a00942c6086ec5005591500
uorri <2405490Gitlub>
2023-03-06 19:18:21 +0300

    Remove name param in create passenger method

--- src/main/kotlin/passenger/PassengerGenerator.kt
+++ src/main/kotlin/passenger/PassengerGenerator.kt
@@ -1,15 +1,18 @@
+package passenger
+
+import building.Building
+import com.github.javafaker.Faker
+
 object PassengerGenerator {
-    fun getPassenger(name: String, sourceFloor: Int, targetFloor: Int): Passenger {
-        if (name.isBlank() || name.isEmpty()) {
-            throw IllegalArgumentException("Invalid name: $name")
-        }
+    private val faker = Faker()
+    fun create(sourceFloor: Int, targetFloor: Int): Passenger {
         if (sourceFloor < Building.firstFloor || sourceFloor > Building.lastFloor) {
             throw IllegalArgumentException("Invalid source floor: $sourceFloor")
         }
         if (targetFloor < Building.firstFloor || targetFloor > Building.lastFloor) {
             throw IllegalArgumentException("Invalid target floor: $sourceFloor")
         }
-        return Passenger(name, sourceFloor, targetFloor)
+        return Passenger(faker.name().firstName(), sourceFloor, targetFloor)
     }
 
 }
