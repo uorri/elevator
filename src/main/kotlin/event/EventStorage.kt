aefa00f04fba0297f957b192f27bdf7f2e40bbde
uorri <2405490Gitlub>
2023-03-25 15:04:39 +0300

    Add synchronization for critical section

--- src/main/kotlin/event/EventStorage.kt
+++ src/main/kotlin/event/EventStorage.kt
@@ -1,25 +1,22 @@
 package event
 
 import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 object EventStorage {
     private val listeners = CopyOnWriteArrayList<Listener>()
+    private val lock = ReentrantLock()
 
-    fun listen(listener: Listener): Boolean {
-        return listeners.add(listener)
-    }
+    fun listen(listener: Listener) = lock.withLock { listeners.add(listener) }
 
-    fun fire(floorNum: Int) {
-        notify(floorNum)
-    }
+    fun fire(floorNum: Int) = lock.withLock { notify(floorNum) }
 
     private fun notify(floorNum: Int) {
         listeners.filter { it.floorNum == floorNum }.forEach { it.onEvent() }
         forget(floorNum)
     }
 
-    private fun forget(floorNum: Int) {
-        listeners.removeIf { it.floorNum == floorNum }
-    }
+    private fun forget(floorNum: Int) = listeners.removeIf { it.floorNum == floorNum }
 
 }
